a14230da2f58dc007475e1272c9dc44e179dbc11
nmogoko <+>
2024-07-02 20:04:53 +0100

    Revised code

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-//  Code your solution in this file!
-// Using the comparison operators so that if the distance from Hq is greater, 
-// we subtract hq distance from it to  get a positive distance value,
-// Otherwise if distanc is less, we subtract the distance from the hq distance to get a positive value
-function distanceFromHqInBlocks(block){
-     let result = (block > 42)? block - 42: (42-block);
-     return result;
-}
-(distanceFromHqInBlocks(43))
+const destination = 42;
+const feet = 264;
 
-// calling the distanceFromHqBocks within the distanceFromHqInFeet
- function distanceFromHqInFeet(block){
-     let feet = distanceFromHqInBlocks(block) * 264
-   return feet
+
+function distanceFromHqInBlocks(start){
+  if (start < destination) {
+return destination - start 
+  } else 
+  {
+    return start - destination 
+  }
  }
+console.log(distanceFromHqInBlocks(60));
 
-distanceFromHqInFeet(43);
-
- //returns the number of feet traveled
- let distanceTravelled;
- function distanceTravelledInFeet(start, destination){
-     if (start > destination){
-         return(start - destination) * 264;
-     } else
- {
-     return(destination - start) * 264 ;
+ function distanceFromHqInFeet(start){
+     return distanceFromHqInBlocks(start) * feet 
  }
- 
-}
-distanceTravelledInFeet(43, 48);
+console.log(distanceFromHqInFeet(60));
 
-  //returns the fare for the customer
- function calculatesFarePrice(start, destination) {
-   distanceTravelledInFeet(start, destination)
-   if (distanceTravelledInFeet(start, destination) <= 400){
-          return(0) 
-   } else if (distanceTravelledInFeet(start, destination) >400 && distanceTravelledInFeet(start, destination)<= 2000){
-          return(2 * distanceTravelledInFeet(start, destination))
-    } else if (distanceTravelledInFeet(start, destination) >2000 && distanceTravelledInFeet(start, destination)<= 2500){
-          return(25)
-    } else {
-          return(`cannot travel that far`);
+  //returns the number of feet traveled
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+return (destination -start) * feet;
+    } else {  
+      return (start - destination) * feet;
     }
- }
- calculatesFarePrice(43, 44);
+}
+
+//returns the fare for the customer
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination)
+  if (distance < 400 ) {
+    return 0;
+  } else if (distance >= 400 && distance <= 2000) {
+      return ((distance - 400) * 2)/ 100;
+  } else if (distance >  2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
